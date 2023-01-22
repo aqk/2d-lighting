f8a207da1e54e0aaa967b934133afc629f23756c
Adam Kelly <338792+aqk@users.noreply.github.com>
2023-01-21 22:27:48 -0800

    Enemy

---
+++ godot-project/Spike.gd
@@ -0,0 +1,28 @@
+extends KinematicBody2D
+
+var speed = 150
+var move_direction = Vector2(0,0)
+var cycle_secs = 4
+var secs = 0.0
+var dir = -0.5
+
+onready var path_follow = get_parent()
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	path_follow = get_parent()
+
+
+func _physics_process(delta):
+	secs += delta
+	if secs > cycle_secs:
+		secs = 0.0
+		dir *= -1
+	#var dir = (int(secs) % cycle_secs) - (cycle_secs)
+	print(delta, dir)
+	move_direction = Vector2(0, -dir)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var movement = move_direction.normalized() * speed
+	#print(movement)
+	move_and_slide(movement)

f03f1540ed75666f0ad3aa0a207bbc429a58b99c
arty <art.yerkes@gmail.com>
2023-01-21 21:10:10 -0800

    Turning at least works

--- godot-project/Player.gd
+++ godot-project/Player.gd
@@ -3,82 +3,80 @@ extends KinematicBody2D
 export var ACCELERATION = 450
 export var FRICTION = 550
 export var MAX_SPEED = 110
-export var ROLL_SPEED = 150
-
-enum {
-	RUN,
-	ROLL,
-	JUMP
-}
-
-onready var animation_tree = $AnimationTree
-onready var animation_state = animation_tree.get("parameters/playback")
+export var ACCEL = 10
+export var DECEL = 10
 
 var velocity = Vector2.ZERO
-var roll_vector = Vector2.DOWN
-var state = RUN
-var roll_finished = false
+var speed = 0
+var speed_target = 0
+var turn = 0
+var turn_target = 0
 
 var last_fps_time = 0
 var fps = 0
 var acc = 0
 
+var pi = 3.141592653
+
+func sigmoid(x):
+	return 1.0 / (1.0 + exp(-x))
+
 func _ready():
-	animation_tree.active = true
+	pass
 
+func _input(event):
+	speed_target = speed
+	turn_target = 0
+	
+	if event.is_action_pressed("brake"):
+		speed_target = 0
+	elif event.is_action_pressed("jump"):
+		speed_target = MAX_SPEED
+	else:
+		speed_target = MAX_SPEED / 5
+	
+	if event.is_action_pressed("left"):
+		turn_target = -100
+	elif event.is_action_pressed("right"):
+		turn_target = 100
+
+func _process(delta):
+	pass
+	
 func _physics_process(delta):
-	acc += delta
-	fps += 1
-	if acc > 1:
-		print("FPS: ", fps)
-		acc = 0
-		fps = 0
-		
-	match state:
-		RUN:	
-			run_state(delta)
-		ROLL:
-			roll_state(delta)
-	
-func run_state(delta):
-	var input_vector = Vector2.ZERO
-	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-	input_vector = input_vector.normalized()
-	
-	if input_vector != Vector2.ZERO:
-		roll_vector = input_vector
-		animation_tree.set("parameters/Idle/blend_position", input_vector)
-		animation_tree.set("parameters/Run/blend_position", input_vector)
-		animation_tree.set("parameters/Jump/blend_position", input_vector)
-		animation_tree.set("parameters/Roll/blend_position", input_vector)
-		animation_state.travel("Run")
-		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+	if abs(turn_target - turn) > 40:
+		if turn_target > turn:
+			turn = turn + 10
+		else:
+			turn = turn - 10
 	else:
-		animation_state.travel("Idle")
-		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+		turn = (turn + turn_target) / 2
 
-	velocity = move_and_slide(velocity)
-	
-	if velocity.length() > 0 && Input.is_action_just_pressed("roll"):
-		state = ROLL
-		roll_finished = false
-	
-func roll_state(delta):
-	if !roll_finished:
-		velocity = roll_vector * ROLL_SPEED
-	else:
-		velocity = velocity * 0.9
-	animation_state.travel("Roll")
-	velocity = move_and_slide(velocity)
+	if speed > speed_target:
+		if speed == 0:
+			speed = MAX_SPEED / 10
+		else:
+			speed += ACCEL
+	if speed < speed_target:
+		if speed <= MAX_SPEED / 10:
+			speed = 0
+		else:
+			speed -= DECEL
 
-func jump_finished():
-	state = RUN
-	velocity = Vector2.ZERO
+	var car_dir = turn / (pi * 100)
+	var car_cos = cos(car_dir)
+	var car_sin = sin(car_dir)
 	
-func roll_prefinished():
-	roll_finished = true
+	var new_velocity_x = car_cos * speed
+	var new_velocity_y = car_sin * speed
 	
-func roll_finished():
-	state = RUN
-	velocity = velocity * 0.5
+	var new_velocity = Vector2(new_velocity_x, new_velocity_y)
+	var vel_diff = new_velocity - velocity
+	
+	# XXX If vel_diff is too large, skid
+	velocity = new_velocity
+	
+	rotation = car_dir
+	
+	position.x += new_velocity_x
+	position.y += new_velocity_y

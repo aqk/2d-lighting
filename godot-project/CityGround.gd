5abb43c7c11e97614228e33ac29fe1106a3205b4
arty <art.yerkes@gmail.com>
2023-02-10 21:09:21 -0800

    3x3 tile grid moving with car

--- godot-project/CityGround.gd
+++ godot-project/CityGround.gd
@@ -1,12 +1,27 @@
 extends Node2D
 
-var WORLD_GRID_STEP = 1250 * 2
+var WORLD_GRID_STEP = 1248 * 2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$"../Player".connect("car_position", self, "_car_position")
+	for i in range(3):
+		for j in range(3):
+			var index = (3 * i) + j
+			var tile_node = self.get_child(index)
+			var node_pos = Vector2((j - 1) * WORLD_GRID_STEP, (i - 1) * WORLD_GRID_STEP)
+			tile_node.position = node_pos
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _car_position(pos):
+	var world_step = WORLD_GRID_STEP
+	var p_x_int = int(pos.x)
+	var p_y_int = int(pos.y)
+	var grid_tile_x = (p_x_int + (world_step / 2)) / world_step
+	var grid_tile_y = (p_y_int + (world_step / 2)) / world_step
+	print(grid_tile_x, ", ", grid_tile_y)
+	for i in range(3):
+		for j in range(3):
+			var index = (3 * i) + j
+			var tile_node = self.get_child(index)
+			var node_pos = Vector2(((j - 1) + grid_tile_x) * world_step, ((i - 1) + grid_tile_y) * world_step)
+			tile_node.position = node_pos
--- godot-project/Dungeon.gd
+++ godot-project/Dungeon.gd
@@ -2,19 +2,7 @@ extends TileMap
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$"../../Player".connect("car_position", self, "_car_position")
-
-# Car at 0, Ground at 0
-# next boundary.
-# therefore: 
-func _car_position(pos):
-	var world_step = $"..".WORLD_GRID_STEP
-	var p_x_int = int(pos.x)
-	var p_y_int = int(pos.y)
-	var grid_tile_x = (p_x_int + (world_step / 2)) / world_step
-	var grid_tile_y = (p_y_int + (world_step / 2)) / world_step
-	print(grid_tile_x, ", ", grid_tile_y)
-	position = Vector2(float(grid_tile_x * world_step), float(grid_tile_y * world_step))
+	 pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):

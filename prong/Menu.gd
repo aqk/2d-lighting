1fc08fbf8e2af0593c55669ba26b84a32e27b84c
arty <art.yerkes@gmail.com>
2023-05-09 23:13:37 -0700

    Fixes

--- prong/Menu.gd
+++ prong/Menu.gd
@@ -6,7 +6,8 @@ extends Control
 func _ready():
 	randomize()
 	for btn in $VBoxContainer.get_children():
-		btn.connect("pressed", self, "_on_button_pressed", [btn.text])
+		if btn is Button:
+			btn.connect("pressed", self, "_on_button_pressed", [btn.text])
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,12 +17,14 @@ func _ready():
 func _on_button_pressed(btn_text):
 	match btn_text:
 		"Host":
-			for btn in $VBoxContainer.get_chidren():
-				btn.disabled = true
+			for btn in $VBoxContainer.get_children():
+				if btn is Button:
+					btn.disabled = true
 			$Network.host_game(false)
 		"Client":
-			for btn in $VBoxContainer.get_chidren():
-				btn.disabled = true
+			for btn in $VBoxContainer.get_children():
+				if btn is Button:
+					btn.disabled = true
 			$Network.join_game()
 		"Start":
 			$Network.server_start_game()

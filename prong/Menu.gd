bcc8986cd34779b7e0072427ec079f1e05934596
Adam Kelly <338792+aqk@users.noreply.github.com>
2023-05-09 23:29:30 -0700

    rename control var

--- prong/Menu.gd
+++ prong/Menu.gd
@@ -5,9 +5,9 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
-	for btn in $VBoxContainer.get_children():
-		if btn is Button:
-			btn.connect("pressed", self, "_on_button_pressed", [btn.text])
+	for control in $VBoxContainer.get_children():
+		if control is Button:
+			control.connect("pressed", self, "_on_button_pressed", [control.text])
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.

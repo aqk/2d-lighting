ba81aac55329131f611293c22976e2cb656cf922
arty <art.yerkes@gmail.com>
2023-05-10 00:18:39 -0700

    paddle control

--- prong/main.gd
+++ prong/main.gd
@@ -1,20 +1,25 @@
 extends Node2D
 
+signal mouse_pos(y)
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var new_position
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	new_position = position
+	pass
 
 func _input(event):
 	if event is InputEventMouse:
-		new_position = event.global_position
+		emit_signal("mouse_pos", event.global_position.y)
+	
+func set_server_mouse_pos(y):
+	$Paddle2.position.y = y
+	
+func set_client_mouse_pos(y):
+	$Paddle2.position.y = y
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	#$Paddle/KinematicBody2D.move_and_slide(Vector2(0, new_position.y-position.y))
-	$Paddle.position = Vector2($Paddle.position.x, new_position.y)
+	pass

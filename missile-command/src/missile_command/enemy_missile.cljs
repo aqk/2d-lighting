5e169cc1912105b7c02e2e3f753e31c74ed7dbf9
Adam Kelly <338792+aqk@users.noreply.github.com>
2023-06-03 16:11:07 -0700

    Remember origin point of enemy missiles

--- missile-command/src/missile_command/enemy_missile.cljs
+++ missile-command/src/missile_command/enemy_missile.cljs
@@ -1,13 +1,14 @@
 
 ; Enemy missiles
 
-(def SPEED 5) ; pixels per frame
-(def all_missiles [])
+(def SPEED 5) ; pixels per simulation tick
+(def enemy_missiles [])
 
-(defn create-missile "Create new missile on-screen"
+(defn create-missile "Create new Enemy missile on-screen"
       [pos target_pos]
-      (set! all_missiles (conj all_missiles {:pos [] :target_pos [] :speed SPEED}))
-      ; todo: return missile?
+      (set! enemy_missiles (conj enemy_missiles
+            {:origin pos :pos pos :target_pos target_pos :speed SPEED})
+      )
 )
 
 ; Note: we may have to destroy other types of enemies in future

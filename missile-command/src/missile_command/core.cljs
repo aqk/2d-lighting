44d2e26951a4ab5efaece8bc0e7c7374ab6609b9
arty <art.yerkes@gmail.com>
2023-06-03 14:22:41 -0700

    Add timer, now we know what time it is in the game

--- missile-command/src/missile_command/core.cljs
+++ missile-command/src/missile_command/core.cljs
@@ -12,13 +12,31 @@
       )
   )
 
+(defn random-color []
+  (str "#" (rand-int 10) "27")
+  )
 
-(let [cc (canvas-context)]
+(defn new-game-state [] {
+  :start (timer/now)
+  })
+
+(defn run-game [cc current-time game-state]
+  (println (- current-time (game-state :start)))
+  (canvas/color-fill cc "#327")
+  (canvas/rect cc 0 0 800 600)
+  (canvas/fill cc)
+  )
+
+(let [cc (canvas-context) global-game-state (atom (new-game-state))]
+  (println global-game-state)
   (canvas/on-frame
    (fn my-task []
-     (canvas/color-fill cc "#327")
-     (canvas/rect cc 0 0 800 600)
-     (canvas/fill cc)
+     (let [current-time (timer/now) game-state @global-game-state]
+       (if game-state
+         (run-game cc current-time game-state)
+         ()
+         )
+       )
      )
    )
   )

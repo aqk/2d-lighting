51fdb73b096fe95cb3538e89839c3ce823f76b14
arty <art.yerkes@gmail.com>
2023-06-03 13:47:46 -0700

    Dependencies working, starting to try drawing on the canvas

--- missile-command/src/missile_command/core.cljs
+++ missile-command/src/missile_command/core.cljs
@@ -1,7 +1,28 @@
-(ns hello-world.core)
+(ns missile-command.core)
+
+(require '[helins.timer :as timer])
+(require '[helins.canvas :as canvas])
 
 (println "Hello world!")
 
+(defn canvas-context []
+  (-> js/document
+      (.getElementById "canvas")
+      (.getContext "2d")
+      )
+  )
+
+
+(timer/every timer/main-thread
+          1000
+          (fn my-task []
+            (let [cc (canvas-context)]
+              (canvas/color-fill cc "#327")
+              (canvas/clear cc 0 0 800 600)
+              )
+            (println "Hello world!")
+            )
+          )
+
 (defn average [a b]
   (/ (+ a b) 2.0))
-

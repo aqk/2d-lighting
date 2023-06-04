93c02eb77873df7b3fc4f6ae468def7d9fec72d6
arty <art.yerkes@gmail.com>
2023-06-03 19:36:46 -0700

    WIP checkpoint

--- missile-command/src/missile_command/player_missile.cljs
+++ missile-command/src/missile_command/player_missile.cljs
@@ -1,18 +1,15 @@
+(ns missile-command.player_missile)
 
 ; Player missiles
 
 (def SPEED 10) ; pixels per frame
-(def player_missiles [])
 
 (defn create-missile "Create new player missile on-screen"
       [pos target_pos]
-      (set! all_missiles (conj all_missiles {:pos [] :target_pos [] :speed SPEED}))
-      ; todo: return missile?
-)
+  {:pos pos :target_pos target_pos :speed SPEED}
+  )
 
 ; Note: we may have to destroy other types of enemies in future
-(def destroy-missiles "Destroy on-screen missiles within given circle"
-     [pos radius]
-     
-)
-     
+;; (def destroy-missiles "Destroy on-screen missiles within given circle"
+;;      [pos radius]
+;; )
--- missile-command/src/missile_command/sim.cljs
+++ missile-command/src/missile_command/sim.cljs
@@ -1,48 +1,61 @@
+(ns missile-command.sim)
+
+(require '[missile-command.util :as util])
+(require '[missile-command.silo :as silo])
+(require '[missile-command.cities :as cities])
+(require '[missile-command.player_missile :as player_missile])
 
 (defn init-game-state []
-      {:enemy_missiles ()
+      {:kind "game"
+       :enemy_missiles ()
        :player_missiles ()
+       :explosions ()
+       :cities (cities/init-cities)
+       :silos (silo/get-silos)
+       :wave 1
        :score 0
-       :cities (init-cities)
-       :silos (all_silos)
       }
 )
 
 ; TODO: Choose enemy missile frequency and spawn points
 
+;; create new objects
+;; if mouse-clicked, try to create a player_missile
+(defn add_new_player_missile_if_clicked [old-game-state mouse-state]
+  (println "mouse-state" mouse-state)
+  (if (get mouse-state :left_click)
+    (let [origin {:x 0 :y 0}] ;; XXX Pick an origin
+      (let [new-missile (player_missile/create-missile origin (get mouse-state :pos))]
+        (let [old-missile-list (old-game-state :player_missiles)]
+          (update old-game-state assoc :player_missiles (cons new-missile old-missile-list))
+          )
+        )
+      old-game-state
+      )
+    )
+  )
+
+(defn move-player-missiles [old-game-state] old-game-state)
+
+(defn move-enemy-missiles [old-game-state] old-game-state)
+
+(defn collide-enemy-missiles-with-cities [old-game-state] old-game-state)
+
+(defn collide-enemy-missiles-with-silos [old-game-state] old-game-state)
+
+(defn collide-player-explosions [old-game-state] old-game-state)
+
 ; returns a new game-stae
 (defn step-game-state [mouse-state old-game-state]
-      ; create new objects
-      ; if mouse-clicked, try to create a player_missile
-      (def new_player_missiles
-           (if (get mouse-state :left_click)
-               (player_missiles/create-missile (get mouse-state :pos))
-               (get old-game-state :enemy_missiles)
-           )
-      )
+  ;; move all objects
+  (->> old-game-state
+       (add_new_player_missile_if_clicked mouse-state)
+       (move-player-missiles)
+       (move-enemy-missiles)
 
-      ; move all objects
-      (set! new_player_missiles (move-player-missiles))
-      (def new_enemy_missiles (move-enemy-missiles))
-
-      ; do collisions
-      (collide-enemy-missiles cities)
-      (collide-enemy-missiles silos)
-      (collide-player-explosions (get new-game-state :enemy_missiles))
-
-      ; TODO: let collisions affect silos, cities
-      ; TODO: let exploding player missles affect enemy missiles
-      (def new_cities (get old-game-state :cities))
-      (def new_silos (get old-game-state :silos))
-
-      ; update score
-      (def new_score score)      
-
-      {:enemy_missiles new_enemy_missiles
-       :player_missiles new_player_missiles
-       :score new_score
-       :cities new_cities
-       :silos new_silos
-      }
-
-)
+       ;; do collisions
+       (collide-enemy-missiles-with-cities)
+       (collide-enemy-missiles-with-silos)
+       (collide-player-explosions)
+       )
+  )
